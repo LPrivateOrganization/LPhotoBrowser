b0601fffd359ad961288331d29e953405552ac17
RNai <mobile@alayy.com>
2016-08-10 14:26:48 +0800

    lcw
    update Demo.

---
+++ LPhotoBrowserDemo/LPhotoBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LPhotoBrowser
+//
+//  Created by lichaowei on 15/12/23.
+//  Copyright © 2015年 lcw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
